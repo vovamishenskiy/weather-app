3761bc1ca9d38590f7d7f87d6f1c3ae0c57e2a7b
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2023-08-26 14:43:36 +0400

    some comments on code

--- src/App.js
+++ src/App.js
@@ -2,35 +2,36 @@ import './App.css';
 import { useState } from 'react'
 import env from 'react-dotenv'
 
-const baseUrl = 'http://api.weatherapi.com/v1'
-
 const App = () => {
+  // creating array of city information and city for input
   const [dataArray, setDataArray] = useState([])
   const [city, setCity] = useState('')
 
+  // handling input change and capitalizing first letter of inputted city for aesthetic purpose
   const handleCityChange = (event) => setCity(event.target.value)
   const capitalizeFirst = city => {
     return city.charAt(0).toUpperCase() + city.slice(1)
   }
 
-
+  // handling button click - asynchronously fetching data from api url using city from input
   const handleBtnClick = async () => {
     try {
-      const response = await fetch(`${baseUrl}/current.json?key=${env.KEY}&q=${city}`, {
+      const response = await fetch(`${env.BASE_URL}/current.json?key=${env.KEY}&q=${city}`, {
         method: 'GET',
         headers: {
           Accept: 'application/json',
         },
       })
 
+      // turning request response into json 
       const res = await response.json()
 
-      // console.log(`res is: ${JSON.stringify(res, null, 4)}`)
-
+      // setting city information array with json'ed response
       setDataArray(res)
     } catch (err) { console.log(err) }
   }
 
+  // handling city clear button to clear city input and city information output
   const handleCityClear = () => {
     setCity('')
     setDataArray([])
@@ -40,6 +41,8 @@ const App = () => {
     <div>
       <input value={city} onChange={handleCityChange}></input> <button onClick={handleBtnClick}>get</button><button onClick={handleCityClear}>clear</button>
       <p>{capitalizeFirst(city)}</p>
+
+      {/* checking if array of city information contains something and outputting it */}
       {dataArray.length !== 0 &&
         <div><pre>{JSON.stringify(dataArray, null, 4)}</pre></div>
       }
@@ -49,12 +52,3 @@ const App = () => {
 }
 
 export default App;
-
-  // useEffect(() => {
-  //   const weatherData = async () => {
-  //     await axios.get(`${baseUrl}/current.json?key=${key}&q=${city}`).then(res => setDataArray(res.data))
-  //   }
-  //   weatherData()
-  // }, [])
-
-  // console.log(dataArray)
